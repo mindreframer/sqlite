99b21b5b20c8c4793fe44981e9affaf9989be305
Roman Heinrich <roman.heinrich@gmail.com>
2022-06-17 20:38:49 +0200

    Update to latest SQLITE version (3.3.8)

--- bin/download.sh
+++ bin/download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export SQLITE_RELEASE_YEAR="2021"
-export SQLITE_VERSION="3360000"
+export SQLITE_RELEASE_YEAR="2022"
+export SQLITE_VERSION="3380500"
 
 curl -L http://sqlite.org/$SQLITE_RELEASE_YEAR/sqlite-amalgamation-$SQLITE_VERSION.zip --output src.zip
 unzip src.zip
